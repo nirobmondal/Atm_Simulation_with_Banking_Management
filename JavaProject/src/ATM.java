04f3482b25a347c1b8b862ae35508bd84f396c50
ShadmanRahmanJU51 <shadman.rahman@gmail.com>
2024-10-20 17:39:56 +0600

    Added atm.java file

---
+++ JavaProject/src/ATM.java
@@ -0,0 +1,20 @@
+package JavaProject.src;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
+public class ATM {
+
+
+    public static void main(String[] args) {
+        InitialWindow initialWindow = new InitialWindow();
+        initialWindow.setVisible(true);
+        initialWindow.setSize(750, 475);
+        initialWindow.setLocation(450, 220);
+        initialWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
+
+
